f87963968fed2714920900ccefa646c8d4baf85c
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-10-18 06:09:42 -0300

    Creada vista Inicio y cambio en RouteConfig para iniciar en la misma.

--- pw3-tpIntegrador/App_Start/RouteConfig.cs
+++ pw3-tpIntegrador/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace pw3_tpIntegrador
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "Home", action = "Inicio", id = UrlParameter.Optional }
 			);
 		}
 	}
