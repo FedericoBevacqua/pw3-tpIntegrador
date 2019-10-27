0b07cae882b55e062614f1fc9e67f59074d3233c
Daniel Marconi <marconidaniel24@gmail.com>
2019-10-27 16:26:48 -0300

    Se crea vista de login

--- pw3-tpIntegrador/Controllers/HomeController.cs
+++ pw3-tpIntegrador/Controllers/HomeController.cs
@@ -12,5 +12,15 @@ namespace pw3_tpIntegrador.Controllers
 		{
 			return View();
 		}
-	}
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+    }
 }
