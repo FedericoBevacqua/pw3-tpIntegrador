3e8fac68ece55b79ed169c937fee1af050cf3e13
Daniel Marconi <marconidaniel24@gmail.com>
2019-10-28 14:15:37 -0300

    Se agrega javascript para la vista de crear propuesta

--- pw3-tpIntegrador/App_Start/BundleConfig.cs
+++ pw3-tpIntegrador/App_Start/BundleConfig.cs
@@ -9,15 +9,18 @@ namespace pw3_tpIntegrador
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery/jquery-{version}.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery/jquery.validate*"));
 
 			// Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
 			// para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
 			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/mainjs").Include(
+                         "~/Scripts/crear-propuesta.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap/bootstrap.js",
