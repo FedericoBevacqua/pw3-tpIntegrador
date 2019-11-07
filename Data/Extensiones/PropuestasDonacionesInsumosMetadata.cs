13be10d6cb3a7b0db97b2c5f6b7609a7a971b593
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-11-07 14:06:25 -0300

    Agregada extensiones PropuestasInsumos

---
+++ Data/Extensiones/PropuestasDonacionesInsumosMetadata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pw3_tpIntegrador
+{
+	public class PropuestasDonacionesInsumosMetadata
+	{
+		[Required(ErrorMessage = "El Nombre no puede estar vacio")]
+		[MaxLength(50, ErrorMessage = "Maximo 50 caracteres")]
+		public string Nombre { get; set; }
+
+		[Required(ErrorMessage = "La Cantidad no puede estar vacio")]
+		public int Cantidad { get; set; }
+	}
+}
