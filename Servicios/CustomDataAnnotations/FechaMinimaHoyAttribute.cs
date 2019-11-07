e77d96e5873c311a954fd5a9b75a41ee7d950436
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-11-07 09:54:01 -0300

    Data Annotation Personalizada FechaMinimaHoy

---
+++ Servicios/CustomDataAnnotations/FechaMinimaHoyAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios.CustomDataAnnotations
+{
+	public class FechaMinimaHoyAttribute : ValidationAttribute
+	{
+		protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+
+		{
+
+			DateTime CurrentDate = DateTime.Now;
+
+			string Message = string.Empty;
+
+			if (Convert.ToDateTime(value) < CurrentDate)
+
+			{
+
+				Message = "La fecha no puede ser menor al dia de hoy";
+
+				return new ValidationResult(Message);
+
+			}
+
+			return ValidationResult.Success;
+
+		}
+	}
+}
