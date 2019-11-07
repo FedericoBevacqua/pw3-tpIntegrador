dc09dd6f1aafadde5a75fca786ea45829c518dcd
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-11-07 14:00:04 -0300

    Creadas Extensiones PropuestasReferencias

---
+++ Data/Extensiones/PropuestasReferenciasMetadata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pw3_tpIntegrador
+{
+	public class PropuestasReferenciasMetadata
+	{
+		[Required(ErrorMessage = "El Nombre no puede estar vacio")]
+		[MaxLength(50, ErrorMessage = "Maximo 50 caracteres")]
+		public string Nombre { get; set; }
+
+		[Required(ErrorMessage = "El Telefono no puede estar vacio")]
+		[MaxLength(50, ErrorMessage = "Maximo 50 caracteres")]
+		public string Telefono { get; set; }
+	}
+}
