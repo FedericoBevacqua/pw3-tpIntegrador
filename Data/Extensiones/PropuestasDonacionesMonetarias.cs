d916def0c8f8f1371cbff536f1e4582527d3637c
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-11-07 12:17:26 -0300

    Creada Extensiones/PropuestaDonacionesMonetarias con DataAnnotations

---
+++ Data/Extensiones/PropuestasDonacionesMonetarias.cs
@@ -0,0 +1,15 @@
+﻿using pw3_tpIntegrador;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+	[MetadataType(typeof(PropuestasDonacionesMonetariasMetadata))]
+	public class PropuestasDonacionesMonetarias
+	{
+	}
+}
---
+++ Data/Extensiones/PropuestasDonacionesMonetariasMetadata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pw3_tpIntegrador
+{
+	public class PropuestasDonacionesMonetariasMetadata
+	{
+		[Required(ErrorMessage = "El monto no puede estar vacio")]
+		[Range(0, 9999999999999999.99, ErrorMessage = "El monto no es valido")]
+		public decimal Dinero { get; set; }
+
+		[Required(ErrorMessage = "El Cbu no puede estar vacio")]
+		[MaxLength(80, ErrorMessage = "Maximo 80 numeros")]
+		public string CBU { get; set; }
+	}
+}
