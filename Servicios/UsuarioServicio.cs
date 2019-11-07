a678be7ffd0acaa214326e0b449c440587c96154
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-11-07 08:27:46 -0300

    Creado servicio Usuario con Alta y Obtener Id

---
+++ Servicios/UsuarioServicio.cs
@@ -0,0 +1,29 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+	public class UsuarioServicio
+	{
+		TpEntities ctx = new TpEntities();
+
+		public void Alta(Usuario u)
+		{
+			u.Token = "12345"; //TODO: Token Autogenerado
+			u.FechaCracion = DateTime.Now;
+			u.Activo = false;
+			u.TipoUsuario = 0; // 0 = Normal | 1 = Administrador 
+			ctx.Usuarios.Add(u);
+			ctx.SaveChanges();
+		}
+
+		public Usuario ObtenerPorId(int id)
+		{
+			return ctx.Usuarios.Find(id);
+		}
+	}
+}
