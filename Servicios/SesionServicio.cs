e11d2bd5df867e6803325e6e79e2ce06ca4cb8f1
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-11-09 11:49:19 -0300

    Creado servicio Sesion

---
+++ Servicios/SesionServicio.cs
@@ -0,0 +1,27 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Servicios
+{
+	public static class SesionServicio
+	{
+		internal static readonly object UsuarioLogin;
+
+		public static Usuario UsuarioSession
+		{
+			get
+			{
+				return HttpContext.Current.Session["UserSession"] as Usuario;
+			}
+			set
+			{
+				HttpContext.Current.Session["UserSession"] = value;
+			}
+		}
+	}
+}
