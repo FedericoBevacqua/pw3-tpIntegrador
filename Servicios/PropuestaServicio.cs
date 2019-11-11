33374c8b018eaf2ba84452c0a448149156ddf7f4
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-11-11 15:09:36 -0300

    ObtenerActivas e CincoMejores (Falta)

--- Servicios/PropuestaServicio.cs
+++ Servicios/PropuestaServicio.cs
@@ -74,8 +74,18 @@ namespace Servicios
         {
             return ctx.Propuestas.ToList();
         }
-
-        public void CrearDenuncia(Denuncia d)
+		public List<Propuesta> ObtenerActivas()
+		{
+			return ctx.Propuestas.Where(x => x.Estado == 1).ToList();
+		}
+		public List<Propuesta> ObtenerCincoMejoresActivas()
+		{
+			//Falta logica 5 
+			//Count? .Count();
+			//return ctx.Propuestas.Where(x => x.Estado == 1).OrderByDescending(x => x.Valoracion).Take(5);
+			return ctx.Propuestas.Where(x => x.Estado == 1).OrderByDescending(x => x.Valoracion).ToList();
+		}
+		public void CrearDenuncia(Denuncia d)
         {
             //Denuncia denuncia = ObtenerPorId(d.IdPropuesta);
 
