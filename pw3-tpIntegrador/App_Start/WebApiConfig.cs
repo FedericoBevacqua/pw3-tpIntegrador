1b86436468ce90d7173dad3eb98684f0328802e7
FedericoBevacqua <fede.el.xeneize@gmail.com>
2019-11-12 10:29:09 -0300

    Create WebApiConfig.cs

---
+++ pw3-tpIntegrador/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace pw3_tpIntegrador
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				//routeTemplate: "api/{controller}/{id}",
+				routeTemplate: "api/{controller}/{action}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Serialize;
+			config.Formatters.JsonFormatter.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+			config.Formatters.XmlFormatter.UseXmlSerializer = true;
+		}
+	}
+}
