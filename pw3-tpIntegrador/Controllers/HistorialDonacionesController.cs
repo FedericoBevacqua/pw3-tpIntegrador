0947af1608dbb7bfb2015acb8cf89a8cbb8c0635
Daniel Marconi <marconidaniel24@gmail.com>
2019-11-16 19:04:06 -0300

    Historial de donaciones por API que se consume por AJAX

---
+++ pw3-tpIntegrador/Controllers/HistorialDonacionesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Servicios;
+using Servicios.DTO.Donacion;
+
+namespace pw3_tpIntegrador.Controllers
+{
+    public class HistorialDonacionesController : ApiController
+    {
+        DonacionServicio _donacionServicio = new DonacionServicio();
+
+        // GET: api/HistorialDonaciones/5
+        public List<DonacionDto> Get(int id)
+        {
+            return _donacionServicio.ObtenerDonacionesDelUsuario(id);
+        }
+    }
+}
