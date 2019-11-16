ecaf2610f65dd2cc4b3aa18bbb01e28a498dfaad
Daniel Marconi <marconidaniel24@gmail.com>
2019-11-16 20:34:41 -0300

    Feedback para el caso que el usuario no tenga donaciones

--- pw3-tpIntegrador/Scripts/historial-donaciones.js
+++ pw3-tpIntegrador/Scripts/historial-donaciones.js
@@ -1,16 +1,21 @@
 ﻿let pedirHistorial = (idUsuario) => {
     const cardSnippet = '<div class="card"><img src="{Foto}" class="card-img-top" alt="Imagen de propuesta" onerror="this.src=\'https://www.loottis.com/wp-content/uploads/2014/10/default-img.gif\'"><div class="card-body"><h5 class="card-title">{Nombre}</h5><div class="card-subtitle mb-2 text-muted">{TipoDonacion}</div><div class="card-text">Donaste <span class="font-weight-bold">{MiDonacion}</span></div><div class="card-text">Total recaudado: {TotalRecaudado}</div><a href="{LinkAPublicacion}" class="btn btn-primary mt-4">Ver publicación</a></div><div class="card-footer"><div>Publicación {Estado}</div></div></div>';
     $.get(window.location.origin + "/api/HistorialDonaciones/" + idUsuario, function (data) {
-        Array.from(data).forEach((e) => {
-            let card = cardSnippet.replace("{Foto}", e.Foto)
-                .replace("{Nombre}", e.Nombre)
-                .replace("{TipoDonacion}", e.TipoDonacion)
-                .replace("{MiDonacion}", e.MiDonacion)
-                .replace("{TotalRecaudado}", e.TotalRecaudado)
-                .replace("{LinkAPublicacion}", e.LinkAPublicacion)
-                .replace("{Estado}", e.Estado);
+        if (data.length !== 0) {
+            Array.from(data).forEach((e) => {
+                let card = cardSnippet.replace("{Foto}", e.Foto)
+                    .replace("{Nombre}", e.Nombre)
+                    .replace("{TipoDonacion}", e.TipoDonacion)
+                    .replace("{MiDonacion}", e.MiDonacion)
+                    .replace("{TotalRecaudado}", e.TotalRecaudado)
+                    .replace("{LinkAPublicacion}", e.LinkAPublicacion)
+                    .replace("{Estado}", e.Estado);
 
-            $('#contenedor-historial-donaciones').append(card);
-        })
+                $('#contenedor-historial-donaciones').append(card);
+            })
+        } else {
+            $('#contenedor-historial-donaciones').append('<div class="alert alert-warning w-100"><strong>Todavía no realizaste ninguna donación. </strong>Cuando hagas una, vas a poder verla en esta sección.</div>')
+        }
+        
     });
 }
